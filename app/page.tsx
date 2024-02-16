ca1697d21b79cdb82f1b2a5b668ceb4e99ac8fa8
nik-lmao <fuxx222.anmeldung@gmail.com>
2024-02-16 11:00:18 +0100

    [💎] Added discord status display

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,57 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
+import { useState } from "react";
+
+
 
 export default function Home() {
+  const [ discordStatus, setDiscordStatus ] = useState("offline");
+  
+
+  function getUserStatus(){
+    fetch("https://api.lanyard.rest/v1/users/969253860508061737")
+      .then((res) => res.json())
+      .then((data) => {
+        setDiscordStatus(data["data"]["discord_status"])
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getUserStatus();
+
+  }, []);
+
+  setInterval(getUserStatus, 10000)
+
+  
+
   return (
     <div className="page">
       <div className="centered profile-page w-[400px] h-[350px] p-[20px] rounded-2xl text-white font-bold">
         <div className="banner rounded-lg w-[100%] h-[100px] mb-[40px]">
-          <Image src="/pfp.jpg" alt="Profile Picture" width={70} height={70} className="rounded-full translate-x-[15px] translate-y-[60px]" draggable={false}/>
+          <div>
+            <Image src="/pfp.jpg" alt="Profile Picture" width={70} height={70} className="rounded-full translate-x-[15px] translate-y-[60px]" draggable={false}/>
+            <span className={`${discordStatus == "online" ? "bg-green-500" : (discordStatus == "idle" ? "bg-yellow-500" : (discordStatus == "dnd" ? "bg-red-500" : (discordStatus == "offline" ? "bg-gray-500" : "bg-red-500")))} h-[20px] w-[20px] rounded-full block translate-x-[62px] translate-y-[42px] border-gray-900 border-[2px]`}></span>
+            {/* Please ignore my nice status system :))*/}
+          </div>
         </div>
-        <div className="rounded-lg px-[10px] py-[5px] textbox mb-[20px]">
-          <h1>Hey, I am <span className="text-blue-500">nik</span>! I am a <span className="text-blue-300">developer</span> from <span className="text-blue-300">Germany</span>! I love <span className="text-blue-300">coding</span>, <span className="text-blue-300">gaming</span> and <span className="text-blue-300">meeting friends</span>!</h1>
-        </div>
-        <div className="rounded-lg px-[10px] py-[5px] textbox">
-          <h1 className="text-white">Check out my <a className="text-blue-500" href="https://github.com/nik-lmao" target="_blank">github profile</a>!</h1>
+        <div>
+          <div className="rounded-lg px-[10px] py-[5px] textbox mb-[20px]">
+            <h1>Hey, I am <span className="text-blue-500">nik</span>! I am a <span className="text-blue-300">developer</span> from <span className="text-blue-300">Germany</span>! I love <span className="text-blue-300">coding</span>, <span className="text-blue-300">gaming</span> and <span className="text-blue-300">meeting friends</span>!</h1>
+          </div>
+          <div className="rounded-lg px-[10px] py-[5px] textbox">
+            <h1 className="text-white">Check out my <a className="text-blue-500" href="https://github.com/nik-lmao" target="_blank">github profile</a>!</h1>
+          </div>
+          <div className="text-center">
+            <h1 className="text-white opacity-[0.8] font-bold translate-y-[95vh]">Made with ❤️ by <a className="text-blue-500" href="https://github.com/nik-lmao" target="_blank">nik-lmao</a>.</h1>
+          </div>
         </div>
       </div>
-      <div className="text-center">
-        <h1 className="text-white opacity-[0.8] font-bold translate-y-[95vh]">Made with ❤️ by <a className="text-blue-500" href="https://github.com/nik-lmao" target="_blank">nik-lmao</a>.</h1>
-      </div>
-      
     </div>
   );
 }
