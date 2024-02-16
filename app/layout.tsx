e0b3b9565219ce503afadea92923be5230e06c48
nik-lmao <fuxx222.anmeldung@gmail.com>
2024-02-16 12:43:49 +0100

    [📝] Edited metadata themecolor

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import type { Viewport } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
-  themeColor: "black",
+  themeColor: "green",
 }
 
 export const metadata: Metadata = {
@@ -31,7 +31,7 @@ export default function RootLayout({
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="400" />
-        <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
+        <meta property="og:image:alt" content="image alt" />
 
       </head>
       <body className={inter.className}>{children}</body>
