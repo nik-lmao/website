14e8968d37578f904038b9549076d9f375e7274f
nik-lmao <fuxx222.anmeldung@gmail.com>
2024-02-16 12:18:25 +0100

    [📝] Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="nik's website" />
+        <meta property="og:description" content="welcome to my website!" />
+        <meta property="og:image" content="https://i.pinimg.com/originals/c3/93/de/c393de62970df6a45ddd7bc65935fb96.gif" />
+        <meta property="og:url" content="https://nik-lmao.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
