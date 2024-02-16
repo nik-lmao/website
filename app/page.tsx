7e493771164f59fd987f7a66f7ddae7b7f3506f5
nik-lmao <fuxx222.anmeldung@gmail.com>
2024-02-16 13:14:41 +0100

    [📝] Updated discord status fetching

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,18 @@ import { useState } from "react";
 
 export default function Home() {
   const [ discordStatus, setDiscordStatus ] = useState("offline");
-  
 
   function getUserStatus(){
     fetch("https://api.lanyard.rest/v1/users/969253860508061737")
       .then((res) => res.json())
       .then((data) => {
-        setDiscordStatus(data["data"]["discord_status"])
+        if(data["data"]["success"] == false){
+          setDiscordStatus("offline")
+        }
+        else {
+          setDiscordStatus(data["data"]["discord_status"])
+        }
+        return
       })
       .catch((err) => {
         console.log(err);
@@ -23,17 +28,13 @@ export default function Home() {
 
   useEffect(() => {
     getUserStatus();
-
+    setInterval(getUserStatus, 20000);
   }, []);
-
-  setInterval(getUserStatus, 10000)
-
-  
 
   return (
     <div className="page">
       <div className="centered profile-page w-[400px] h-[350px] p-[20px] rounded-2xl text-white font-bold">
-        <div className="banner rounded-lg w-[100%] h-[100px] mb-[40px]">
+        <div className="banner rounded-lg w-[100%] h-[100px] mb-[50px] border-[3px] border-black">
           <div>
             <Image src="/pfp.jpg" alt="Profile Picture" width={70} height={70} className="rounded-full translate-x-[15px] translate-y-[60px]" draggable={false}/>
             <span className={`${discordStatus == "online" ? "bg-green-500" : (discordStatus == "idle" ? "bg-yellow-500" : (discordStatus == "dnd" ? "bg-red-500" : (discordStatus == "offline" ? "bg-gray-500" : "bg-red-500")))} h-[20px] w-[20px] rounded-full block translate-x-[62px] translate-y-[42px] border-gray-900 border-[2px]`}></span>
@@ -41,10 +42,10 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <div className="rounded-lg px-[10px] py-[5px] textbox mb-[20px]">
+          <div className="rounded-lg px-[10px] py-[5px] textbox mb-[20px] border-black border-[2px] hover:scale-[1.03] transition-all">
             <h1>Hey, I am <span className="text-blue-500">nik</span>! I am a <span className="text-blue-300">developer</span> from <span className="text-blue-300">Germany</span>! I love <span className="text-blue-300">coding</span>, <span className="text-blue-300">gaming</span> and <span className="text-blue-300">meeting friends</span>!</h1>
           </div>
-          <div className="rounded-lg px-[10px] py-[5px] textbox">
+          <div className="rounded-lg px-[10px] py-[5px] textbox border-black border-[2px] hover:scale-[1.03] transition-all">
             <h1 className="text-white">Check out my <a className="text-blue-500" href="https://github.com/nik-lmao" target="_blank">github profile</a>!</h1>
           </div>
         </div>
