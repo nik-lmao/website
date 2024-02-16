2a0351a421a605d1c69208717f7b7535a98ca8a9
nik-lmao <fuxx222.anmeldung@gmail.com>
2024-02-16 18:15:06 +0100

    [💎] Added screen width limit

--- app/page.tsx
+++ app/page.tsx
@@ -33,23 +33,28 @@ export default function Home() {
 
   return (
     <div className="page">
-      <div className="centered profile-page w-[400px] h-[350px] p-[20px] rounded-2xl text-white font-bold">
-        <div className="banner rounded-lg w-[100%] h-[100px] mb-[50px] border-[3px] border-black">
+      <div className="mainPage">
+        <div className="centered profile-page w-[400px] h-[350px] p-[20px] rounded-2xl text-white">
+          <div className="banner rounded-lg w-[100%] h-[100px] mb-[50px] border-[3px] border-black">
+            <div>
+              <Image src="/pfp.jpg" alt="Profile Picture" width={70} height={70} className="rounded-full translate-x-[15px] translate-y-[60px]" draggable={false}/>
+              <span className={`${discordStatus == "online" ? "bg-green-500" : (discordStatus == "idle" ? "bg-yellow-500" : (discordStatus == "dnd" ? "bg-red-500" : (discordStatus == "offline" ? "bg-gray-500" : "bg-red-500")))} h-[20px] w-[20px] rounded-full block translate-x-[62px] translate-y-[42px] border-gray-900 border-[2px]`}></span>
+              {/* Please ignore my nice status system :))*/}
+            </div>
+          </div>
           <div>
-            <Image src="/pfp.jpg" alt="Profile Picture" width={70} height={70} className="rounded-full translate-x-[15px] translate-y-[60px]" draggable={false}/>
-            <span className={`${discordStatus == "online" ? "bg-green-500" : (discordStatus == "idle" ? "bg-yellow-500" : (discordStatus == "dnd" ? "bg-red-500" : (discordStatus == "offline" ? "bg-gray-500" : "bg-red-500")))} h-[20px] w-[20px] rounded-full block translate-x-[62px] translate-y-[42px] border-gray-900 border-[2px]`}></span>
-            {/* Please ignore my nice status system :))*/}
-          </div>
-        </div>
-        <div>
-          <div className="rounded-lg px-[10px] py-[5px] textbox mb-[20px] border-black border-[2px] hover:scale-[1.03] transition-all">
-            <h1>Hey, I am <span className="text-blue-500">nik</span>! I am a <span className="text-blue-300">developer</span> from <span className="text-blue-300">Germany</span>! I love <span className="text-blue-300">coding</span>, <span className="text-blue-300">gaming</span> and <span className="text-blue-300">meeting friends</span>!</h1>
-          </div>
-          <div className="rounded-lg px-[10px] py-[5px] textbox border-black border-[2px] hover:scale-[1.03] transition-all">
-            <h1 className="text-white">Check out my <a className="text-blue-500" href="https://github.com/nik-lmao" target="_blank">github profile</a>!</h1>
+            <div className="p-0 rounded-lg px-[10px] py-[5px] textbox mb-[20px] border-black border-[2px] hover:scale-[1.03] transition-all">
+              <h1>Hey, I am <span className="text-blue-500 font-bold">nik</span>! I am a <span className="text-blue-300 font-bold">developer</span> from <span className="text-blue-300 font-bold">Germany</span>! I love <span className="text-blue-300 font-bold">coding</span>, <span className="text-blue-300 font-bold">gaming</span> and <span className="text-blue-300 font-bold">meeting friends</span>!</h1>
+            </div>
+            <div className="rounded-lg px-[10px] py-[5px] textbox border-black border-[2px] hover:scale-[1.03] transition-all">
+              <h1>Check out my <a className="text-blue-500 font-bold" href="https://github.com/nik-lmao" target="_blank">github profile</a>!</h1>
+            </div>
           </div>
         </div>
       </div>
+      <div className="hidden centered text-center smallScreenAlert">
+        <h1 className="text-white text-xl white-shadow"><span className="text-blue-500 blue-shadow font-bold">Sorry</span>, this page is only available for users with a screen width of <span className="text-blue-500 blue-shadow font-bold">410 pixels and more</span>.</h1>
+      </div>    
     </div>
   );
 }
